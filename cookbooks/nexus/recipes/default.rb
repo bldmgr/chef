f9559d86f17dad4b19e655c84ab1b1690364755c
Mike Montpetit <mimontpe+cisco@cisco.com>
2025-09-16 11:09:58 -0400

    version 4 update

--- cookbooks/nexus/recipes/default.rb
+++ cookbooks/nexus/recipes/default.rb
@@ -59,7 +59,7 @@ end
 execute 'extract_nexus' do
   command <<-EOH
     tar -xzf #{Chef::Config[:file_cache_path]}/#{nexus_package} -C /tmp
-    find "/tmp/nexus-#{node['nexus']['version']}" -mindepth 1 -maxdepth 1 -exec mv {} "#{node['nexus']['home']}";
+    mv "/tmp/nexus-#{node['nexus']['version']}" "#{node['nexus']['home']}"
     chown -R #{node['nexus']['user']}:#{node['nexus']['group']} #{node['nexus']['home']}
     chown -R #{node['nexus']['user']}:#{node['nexus']['group']} #{node['nexus']['data_dir']}
   EOH
